a6d9aec7198e9131f3be4326feb4fe7fd717976e
Torres Laptop <Torres Laptop@Dell-Laptop>
2017-10-03 19:12:11 -0700

    :)

---
+++ Assignment#1/src/Torres.java
@@ -0,0 +1,20 @@
+
+public class Torres {
+
+	/**
+	 * The class defines object data type and methods - it creates objects
+	 * @param args
+	 */
+	
+	public static void main(String args []) {
+		/**
+		 * The main method is used to start the execution of the Java program
+		 */
+	System.out.println("Hello! "); 
+
+	System.out.println("How are you? "); }
+		/**
+		 * Statements are used to show text- when the code is ran, the statement in the parenthesis will show up
+		 */
+			
+	}
