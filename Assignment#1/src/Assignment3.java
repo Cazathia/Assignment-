483bc6692dcc63af52f2e8171af5406a585987e2
Torres Laptop <Torres Laptop@Dell-Laptop>
2017-10-16 22:48:27 -0700

    konbanwa

---
+++ Assignment#1/src/Assignment3.java
@@ -0,0 +1,39 @@
+
+public class Assignment3 {
+public static void main (String[]args) {
+int a= 7; 
+int b= 3; 
+
+if(a<b) {
+	System.out.println(a); 
+}else if(a>b){
+	 System.out.println(b);
+}
+int c= 1; 
+if(a<b) {
+	System.out.println(b);
+}else if(a>b) { 
+	System.out.println(a);
+}else if(c<b) {
+	System.out.println(b);
+}else if(c>b) {
+	System.out.println(c);
+}else if(c<a) {
+	System.out.println(a);
+}else if(c>a) {
+	System.out.println(c);
+}
+int d= 5; 
+while(d>2) {
+	System.out.println(d);
+	d--; 
+}	
+for(int e=15; e<19; e++) {
+	System.out.println(e);
+}
+
+for(int f=27; f>25; f--) {
+	System.out.println(f);
+}
+}
+}
